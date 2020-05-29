49a7e46f110e7c0485f9dae2f89f047c5a22a3b8
aditulsyan <aditya.tulsyan@cleartax.in>
2020-05-30 01:06:08 +0530

    single page application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,28 +7,49 @@ import Header from './HeaderComponent';
 import {DISHES} from '../shared/dishes';
 import Footer from './FooterComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      dishes: DISHES,
-    };
-  }
+    constructor(props){
+        super(props);
+        this.state={
+        dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS,
+        };
+    }
 
   render(){
       const HomePage = () => {
       return(
-          <Home/>
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]} //filter filters out a particular attribute from the array
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
     }
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     return(
     <div>
         <Header/>
             <Switch>
                 <Route path='/home' component={HomePage} />
                 <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}/>}/>
-                <Redirect to="/home" />
+                <Route path='/menu/:dishId' component={DishWithId} />
+                <Route exact path='/contactus' component={Contact} />} />
+                <Redirect to="/home"/>
             </Switch>
         <Footer/>
     </div>
