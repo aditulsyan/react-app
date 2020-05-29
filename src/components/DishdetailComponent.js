24ee91ff62bf380a5f922e59882c2db4179ddad8
aditulsyan <aditya.tulsyan@cleartax.in>
2020-05-29 14:44:28 +0530

    functional components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,13 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
-class DishDetail extends Component{
-    // eslint-disable-next-line no-useless-constructor
-    constructor(props) {
-        super(props);
-    }
-
-    renderDish(dish) {
+//user defined components start with capital letter
+    function RenderDish({dish}) {
         if (dish != null)
             return(
                 <Card >
@@ -25,7 +20,7 @@ class DishDetail extends Component{
             );
     }
 
-    renderComments(comments){
+    function RenderComments({comments}){
         if (comments !== null) {
             const cmnts = comments.map(comment => {
             return (
@@ -58,16 +53,16 @@ class DishDetail extends Component{
         }
     }
 
-    render(){
-        if (this.props.dish != null)
+    const DishDetail = (props)=>{
+        if (props.dish != null)
             return(
                 <div className= "container">
                 <div className = "row">
                     <div className = "col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
+                        <RenderDish dish={props.dish}/>
                     </div>
                     <div className = "col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.dish.comments)}
+                        <RenderComments comments={props.dish.comments}/>
                     </div>
                 </div>
                 </div>
@@ -78,6 +73,4 @@ class DishDetail extends Component{
                 </div>
             );
         }
-}
-
 export default DishDetail
